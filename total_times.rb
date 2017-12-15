9ffd342b5590e9ded91a44e7c0a2094b2a780dc6
jeffrothwell <rothwelljeff@gmail.com>
2017-12-15 12:13:12 -0500

    complete total_times exercise

--- total_times.rb
+++ total_times.rb
@@ -1,4 +1,4 @@
-# ok so I'll admit I couldn't think of how to do this so this is very much taken from the solution
+# ok so I'll admit I couldn't think of how to do this on so this is very much taken from the solution
 # but I will add comments to show I at least know what's going on and am now writing this
 # without looking at the solution directly.
 
@@ -10,17 +10,21 @@ die2 = [1,2,3,4,5,6]
 # need an empty hash
 total_times = {}
 
-# now populate the keys with all possible dice totals, and the values at zero
-# I will use a range for this .each method's argument
+# now populate this hash's keys with all possible dice totals, and the values at zero
+# I will use a range for this .each method
 
 (2..12).each { |sum| total_times[sum] = 0 }
 
-puts total_times
+# time to make a couple of nested .each methods on my die arrays from above
+# and at each iteration, add 1 to the value associated with the key represented
+# by the total of both dice
 
-# die1.each do |d1|
-#   die2.each do |d2|
-#     puts "Dice Roll: #{d1}, #{d2}  Total:#{d1 + d2}"
-#   end
-# end
+die1.each do |d1|
+  die2.each do |d2|
+    total_times[d1 + d2] += 1
+  end
+end
+
+total_times.each { |sum, freq| puts "#{sum} occurs #{freq} times"}
 #
 # puts "There are #{total_permutations} possible permutations"
